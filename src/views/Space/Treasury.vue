10e765b9cce704089e3b0739e8907c96fcfa97d8
bonustrack <fabien@bonustrack.co>
2022-04-29 19:12:42 +0700

    Use Covalent for treasury

--- src/views/Space/Treasury.vue
+++ src/views/Space/Treasury.vue
@@ -1,74 +1,33 @@
 <script setup>
 import { onMounted, ref } from 'vue';
-import { formatUnits } from '@ethersproject/units';
-import { getAddress } from '@ethersproject/address';
 import snapshot from '@snapshot-labs/snapshot.js';
-import networks from '@snapshot-labs/snapshot.js/src/networks.json';
+import { formatUnits } from '@ethersproject/units';
 import spaces from '@/helpers/spaces.json';
-import { _n, shorten } from '@/helpers/utils';
+import { _n, shorten, explorerUrl } from '@/helpers/utils';
 
 defineProps({ space: Object });
 
-const balances = ref({});
+const assets = ref([]);
 const loading = ref(false);
-
-async function getUsdValue(tokens) {
-  const addressesStr = tokens.join(',');
-  const url = `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${addressesStr}&vs_currencies=usd`;
-  const values = await snapshot.utils.getJSON(url);
-  return Object.fromEntries(
-    Object.entries(values).map(value => [getAddress(value[0]), value[1].usd])
-  );
-}
-
-const { getProvider, Multicaller } = snapshot.utils;
-const network = '1';
-const provider = getProvider(network);
-const abi = [
-  'function getEthBalance(address addr) public view returns (uint256 balance)',
-  'function balanceOf(address account) view returns (uint256)',
-  'function decimals() view returns (uint8)',
-  'function name() view returns (string)',
-  'function symbol() view returns (string)'
-];
-
-const tokens = [
-  '0xba100000625a3754423978a60c9317c58a424e3D',
-  '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-  '0xDe30da39c46104798bB5aA3fe8B9e0e1F348163F',
-  '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-];
 
 onMounted(async () => {
   const treasury = spaces.pasta.wallets[0];
   loading.value = true;
-  const prices = await getUsdValue(tokens);
-  const multi = new Multicaller(network, provider, abi);
-  const base = [];
-  base.push({
-    name: 'Ether',
-    symbol: 'ETH',
-    address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-  });
-  multi.call('[0].balanceFn', networks[network].multicall, 'getEthBalance', [
-    treasury
-  ]);
-  tokens.forEach((token, i) => {
-    base[i + 1] = { address: token };
-    multi.call(`[${i + 1}].balanceFn`, token, 'balanceOf', [treasury]);
-    multi.call(`[${i + 1}].name`, token, 'name');
-    multi.call(`[${i + 1}].symbol`, token, 'symbol');
-    multi.call(`[${i + 1}].decimals`, token, 'decimals');
-  });
-  balances.value = (await multi.execute(base)).map(balance => {
-    balance.balance = formatUnits(balance.balanceFn, balance.decimals);
-    if (prices[balance.address]) {
-      balance.price = prices[balance.address];
-      balance.value = balance.price * balance.balance;
-    }
-    console.log('Balance', balance);
-    return balance;
-  });
+  const key = 'ckey_2d082caf47f04a46947f4f212a8';
+	const url = `https://api.covalenthq.com/v1/1/address/${treasury}/balances_v2/?quote-currency=USD&format=JSON&nft=false&no-nft-fetch=true&key=${key}`;
+	const results = await snapshot.utils.getJSON(url);
+  const ether = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+  const etherItem = results.data.items.find(
+    item => item.contract_address === ether
+  );
+  assets.value = [
+    etherItem,
+    ...results.data.items.filter(
+      item =>
+        formatUnits(item.balance || 0, item.contract_decimals || 0) > 0.001 &&
+        item.contract_address !== ether
+    )
+  ];
   loading.value = false;
 });
 </script>
@@ -79,7 +38,11 @@ onMounted(async () => {
       <div class="border-b flex px-4 py-2">
         <h4>Wallet(s)</h4>
       </div>
-      <a class="px-4 py-3 block">
+      <a
+        :href="explorerUrl('1', spaces.pasta.wallets[0])"
+        target="_blank"
+        class="px-4 py-3 block"
+      >
         <h4>
           <Stamp
             type="avatar"
@@ -94,21 +57,27 @@ onMounted(async () => {
     </div>
     <div class="x-block">
       <div class="border-b flex px-4 py-2">
-        <h4>Assets</h4>
+        <h4>Asset(s)</h4>
       </div>
       <UiLoading v-if="loading" class="px-4 py-3 block" />
       <div
-        v-for="(b, i) in balances"
+        v-for="(asset, i) in assets"
         :key="i"
         class="px-4 py-3 border-b last:border-0 flex"
       >
-        <Stamp type="token" :id="b.address" :size="44" class="mr-2" />
+        <Stamp
+          type="token"
+          :id="asset.contract_address"
+          :size="32"
+          class="mr-3 mt-2"
+        />
         <div class="flex-auto">
           <div class="leading-[24px]">
             <div class="text-md text-skin-link">
-              {{ _n(b.balance) }} {{ b.symbol }}
+              {{ _n(formatUnits(asset.balance || 0, asset.contract_decimals || 0)) }}
+              {{ asset.contract_ticker_symbol }}
             </div>
-            <div v-if="b.value">${{ _n(b.value) }}</div>
+            <div>${{ _n(asset.quote) }}</div>
           </div>
         </div>
         <IH-arrow-sm-right class="mt-2" />
