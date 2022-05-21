0754b6c0c45426ebbe0c3450255d3ae2c527d24d
bonustrack <fabien@bonustrack.co>
2022-05-21 16:06:57 +0700

    Change relayer url

--- src/helpers/clients.ts
+++ src/helpers/clients.ts
@@ -1,7 +1,7 @@
 import { Clients } from '@snapshot-labs/sx';
 
 // @ts-ignore
-const url: string = import.meta.env.VITE_RELAYER_URL || 'http://localhost:3000';
+const url: string = import.meta.env.VITE_MANA_URL || 'http://localhost:3000';
 const clients: any = {};
 clients.EthereumSig = new Clients.EthereumSig(url);
 clients.StarkNetSig = new Clients.StarkNetSig(url);
