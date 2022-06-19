9e28fa5a6d4f9e844d748867f8fa4d6cadf915a9
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-06-19 12:02:41 +0200

    fix: prevent assets showing up if loading

--- src/components/Block/TokenPicker.vue
+++ src/components/Block/TokenPicker.vue
@@ -29,30 +29,34 @@ const filteredAssets = computed(() =>
   <div v-if="loading" class="px-4 py-3 block flex justify-center">
     <UiLoading />
   </div>
-  <div v-if="!loading && filteredAssets.length === 0" class="text-center py-2">
-    No results
-  </div>
-  <div
-    v-for="(asset, i) in filteredAssets"
-    :key="i"
-    role="button"
-    class="px-3 py-1 border-b last:border-0 flex justify-between"
-    @click="emit('pick', asset.contract_address)"
-  >
-    <div class="flex items-center">
-      <Stamp type="token" :id="asset.contract_address" :size="32" />
-      <div class="flex flex-col ml-3 leading-[20px] text-skin-link text-sm">
-        <span>
-          {{ asset.contract_ticker_symbol }}
+  <template v-else>
+    <div v-if="filteredAssets.length === 0" class="text-center py-2">
+      No results
+    </div>
+    <div
+      v-for="(asset, i) in filteredAssets"
+      :key="i"
+      role="button"
+      class="px-3 py-1 border-b last:border-0 flex justify-between"
+      @click="emit('pick', asset.contract_address)"
+    >
+      <div class="flex items-center">
+        <Stamp type="token" :id="asset.contract_address" :size="32" />
+        <div class="flex flex-col ml-3 leading-[20px] text-skin-link text-sm">
+          <span>
+            {{ asset.contract_ticker_symbol }}
+          </span>
+          <span>{{ asset.contract_name }}</span>
+        </div>
+      </div>
+      <div class="flex flex-col items-end text-sm">
+        <span class="text-skin-link">
+          {{
+            _n(formatUnits(asset.balance || 0, asset.contract_decimals || 0))
+          }}
         </span>
-        <span>{{ asset.contract_name }}</span>
+        <span>${{ _n(asset.quote) }}</span>
       </div>
     </div>
-    <div class="flex flex-col items-end text-sm">
-      <span class="text-skin-link">
-        {{ _n(formatUnits(asset.balance || 0, asset.contract_decimals || 0)) }}
-      </span>
-      <span>${{ _n(asset.quote) }}</span>
-    </div>
-  </div>
+  </template>
 </template>
