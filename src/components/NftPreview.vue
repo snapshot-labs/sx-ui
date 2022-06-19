7e5cc9c14cadc021da65edcd34b37e06bb667a2a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-06-19 23:08:09 +0200

    feat: add NFT picker (#161)
    * feat: add NFT picker
    
    * feat: use cdn.stamp.fyi
    
    * fix: use better nft ID
    
    * fix: optimize fallback size

---
+++ src/components/NftPreview.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  item: Object,
+  size: Number
+});
+
+function handleError(e) {
+  const size = props.size ? props.size * 2 : 256;
+
+  e.target.src = `https://cdn.stamp.fyi/token/${props.item.contract_address}?s=${size}`;
+}
+</script>
+
+<template>
+  <img
+    :src="item.image"
+    @error="handleError"
+    class="aspect-square rounded"
+    :style="
+      size
+        ? {
+            width: `${size}px`,
+            height: `${size}px`
+          }
+        : {}
+    "
+  />
+</template>
---
+++ src/composables/useNfts.ts
@@ -0,0 +1,49 @@
+import { ref, computed, Ref } from 'vue';
+import snapshot from '@snapshot-labs/snapshot.js';
+
+const nfts: Ref<any[]> = ref([]);
+const loading = ref(true);
+const loaded = ref(false);
+
+export function useNfts() {
+  async function loadNfts(address) {
+    loading.value = true;
+    const key = 'ckey_2d082caf47f04a46947f4f212a8';
+    const url = `https://api.covalenthq.com/v1/1/address/${address}/balances_v2/?quote-currency=USD&format=JSON&nft=true&key=${key}`;
+    const results = await snapshot.utils.getJSON(url);
+    nfts.value = results.data.items
+      .filter(item => item.type === 'nft' && item.nft_data)
+      .map(item => {
+        const tokenId = item.nft_data[0]?.token_id;
+        const title =
+          item.nft_data[0]?.external_data?.name ??
+          item.contract_name ??
+          'Untitled';
+        const displayTitle =
+          title.match(/(#[0-9]+)$/) || !tokenId
+            ? title
+            : `${title} #${tokenId}`;
+
+        const image =
+          item?.nft_data[0]?.external_data.image ??
+          `https://cdn.stamp.fyi/token/${item.contract_address}?s=256`;
+
+        return {
+          ...item,
+          id: `${item.contract_address}:${tokenId || 0}`,
+          tokenId,
+          title,
+          displayTitle,
+          image
+        };
+      });
+    loading.value = false;
+    loaded.value = true;
+  }
+
+  const nftsMap = computed(
+    () => new Map(nfts.value.map(asset => [asset.id, asset]))
+  );
+
+  return { loading, loaded, nfts, nftsMap, loadNfts };
+}
