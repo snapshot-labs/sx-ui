7000b632a28db6ffca3b7b9421b009cd6ae5b38d
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-07-16 15:36:14 +0200

    feat: check int256/uint256 range

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,5 +1,12 @@
 import Ajv from 'ajv';
 import { isAddress } from '@ethersproject/address';
+import {
+  Zero,
+  MinInt256,
+  MaxInt256,
+  MaxUint256
+} from '@ethersproject/constants';
+import { BigNumber } from '@ethersproject/bignumber';
 
 export function validateForm(schema, form) {
   const ajv = new Ajv({ allErrors: true });
@@ -9,7 +16,29 @@ export function validateForm(schema, form) {
   });
 
   ajv.addFormat('uint256', {
-    validate: value => !!value.match(/^[0-9]+$/)
+    validate: value => {
+      if (!value.match(/^([0-9]|[1-9][0-9]+)$/)) return false;
+
+      try {
+        const number = BigNumber.from(value);
+        return number.gte(Zero) && number.lte(MaxUint256);
+      } catch {
+        return false;
+      }
+    }
+  });
+
+  ajv.addFormat('int256', {
+    validate: value => {
+      if (!value.match(/^-?([0-9]|[1-9][0-9]+)$/)) return false;
+
+      try {
+        const number = BigNumber.from(value);
+        return number.gte(MinInt256) && number.lte(MaxInt256);
+      } catch {
+        return false;
+      }
+    }
   });
 
   ajv.validate(schema, form);
@@ -20,7 +49,7 @@ export function validateForm(schema, form) {
   }
 
   for (const error of ajv.errors) {
-    const path = error.dataPath.split('.').slice(1);
+    const path = error.instancePath.split('/').slice(1);
 
     let current = output;
     for (let i = 0; i < path.length - 1; i++) {
