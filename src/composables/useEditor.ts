dae0bc9c05f78fd7b183dc45d0c67e7b4e53ab6b
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-07-17 23:54:59 +0200

    feat: ignore empty drafts

--- src/composables/useEditor.ts
+++ src/composables/useEditor.ts
@@ -1,14 +1,32 @@
-import { reactive, watch } from 'vue';
+import { reactive, watch, computed } from 'vue';
 import { lsGet, lsSet } from '@/helpers/utils';
 
 const proposals = reactive(lsGet('proposals', {}));
 
+function removeEmpty(proposals) {
+  return Object.entries(proposals).reduce(
+    (acc, [id, proposal]: [string, any]) => {
+      if (Object.keys(proposal).length < 2 && proposal.execution.length === 0) {
+        return acc;
+      }
+
+      return {
+        ...acc,
+        [id]: proposal
+      };
+    },
+    {}
+  );
+}
+
 export function useEditor() {
-  watch(proposals, () => lsSet('proposals', proposals));
+  watch(proposals, () => lsSet('proposals', removeEmpty(proposals)));
+
+  const drafts = computed(() => removeEmpty(proposals));
 
   function removeDraft(key) {
     delete proposals[key];
   }
 
-  return { proposals, removeDraft };
+  return { proposals, drafts, removeDraft };
 }
