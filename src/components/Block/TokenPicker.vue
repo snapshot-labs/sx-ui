d00ed3f7127a03866c2b6f813aa4f9ba9e4d3c5b
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-07-24 12:49:22 +0200

    feat: add contract address based search in token picker (#228)
    * feat: add contract address based search in token picker
    
    * feat: use exact, case insensitive match for address

--- src/components/Block/TokenPicker.vue
+++ src/components/Block/TokenPicker.vue
@@ -23,7 +23,9 @@ const filteredAssets = computed(() =>
           .includes(props.searchValue.toLocaleLowerCase()) ||
         asset.contract_name
           .toLocaleLowerCase()
-          .includes(props.searchValue.toLocaleLowerCase())
+          .includes(props.searchValue.toLocaleLowerCase()) ||
+        asset.contract_address.toLocaleLowerCase() ===
+          props.searchValue.toLocaleLowerCase()
       );
     })
     .sort((a, b) => {
