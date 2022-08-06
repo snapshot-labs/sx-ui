18002c376e25e1d015e13d0ace69ec44f0a00831
bonustrack <fabien@bonustrack.co>
2022-08-06 15:56:14 +0700

    Init simulation with Tenderly

---
+++ src/helpers/tenderly.ts
@@ -0,0 +1,35 @@
+import space from '@/helpers/space.json';
+
+const TENDERLY_ACCESS_KEY = import.meta.env.VITE_TENDERLY_ACCESS_KEY;
+const TENDERLY_USER = import.meta.env.VITE_TENDERLY_USER;
+
+export async function simulate(tx) {
+  const url = `https://api.tenderly.co/api/v1/account/me/project/project/simulate`;
+  const body = {
+    network_id: space.network,
+    from: space.wallet,
+    to: tx.to,
+    input: tx.data,
+    gas: 80000,
+    gas_price: '0',
+    value: tx.value,
+    save_if_fails: true
+  };
+  const init = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-Access-Key': TENDERLY_ACCESS_KEY
+    },
+    body: JSON.stringify(body)
+  };
+  const res = await fetch(url, init);
+  const json = await res.json();
+  console.log('Status', json.simulation.status);
+  console.log(
+    `https://dashboard.tenderly.co/${TENDERLY_USER}/project/simulator/${json.simulation.id}`
+  );
+  console.log('Error', json.transaction.error_message);
+  return json;
+}
