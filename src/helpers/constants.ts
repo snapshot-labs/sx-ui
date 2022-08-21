8c68b0d761061aeaf02e81c00dcef834c583bf1c
bonustrack <fabien@bonustrack.co>
2022-08-21 23:40:38 +0700

    Display auth, voting, executor strategies on settings

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,1 +1,18 @@
 export const ETH_CONTRACT = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+
+export const AUTHS = {
+  '0x594a81b66c3aa2c64577916f727e1307b60c9d6afa80b6f5ca3e3049c40f643':
+    'Vanilla',
+  '0x6ad07205a4d725c5c2b10c4f5fbdfaaa351c742fce7a5a22b2b56fd8d5afd62':
+    'Ethereum signature'
+};
+
+export const STRATEGIES = {
+  '0x344a63d1f5cd0e5f707fede9886d5dd306e86eba91ea410b416f39e44c3865': 'Vanilla',
+  '0x4bbd8081b1e9ef84ee2a767ef2cdcdea0dd8298b8e2858afa06bed1898533e6':
+    'Single slot proof'
+};
+
+export const EXECUTORS = {
+  '0x7402b474327a0d7a2d5c3e01489386113d9654eaff8591344577458074bb1b7': 'Vanilla'
+};
--- src/views/Space/Voting.vue
+++ src/views/Space/Voting.vue
@@ -7,29 +7,29 @@ defineProps({ space: Object });
 <template>
   <Container>
     <h2 class="mb-3">Voting</h2>
-    <div class="mb-2">
+    <div class="mb-3">
       <div class="s-label">Voting delay</div>
       <div
         class="text-skin-link"
         v-text="_d(space.voting_delay) || 'No delay'"
       />
     </div>
-    <div class="mb-2">
+    <div class="mb-3">
       <div class="s-label">Min. voting period</div>
       <div
         class="text-skin-link"
         v-text="_d(space.min_voting_period) || 'No min.'"
       />
     </div>
-    <div class="mb-2">
+    <div class="mb-3">
       <div class="s-label">Max. voting period</div>
       <div class="text-skin-link" v-text="_d(space.max_voting_period)" />
     </div>
-    <div class="mb-2">
+    <div class="mb-3">
       <div class="s-label">Proposal threshold</div>
       <div class="text-skin-link" v-text="space.proposal_threshold" />
     </div>
-    <div class="mb-2">
+    <div class="mb-3">
       <div class="s-label">Quorum</div>
       <div class="text-skin-link" v-text="space.quorum" />
     </div>
