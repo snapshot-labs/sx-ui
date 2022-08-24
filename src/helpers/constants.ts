b62c4c550196882ab7670d402f913b7615e0a67a
Fabien <bonustrack@users.noreply.github.com>
2022-08-24 18:39:07 +0700

    Update src/helpers/constants.ts
    Co-authored-by: Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,9 +1,9 @@
 export const ETH_CONTRACT = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 
 export const AUTHS = {
-  '0x594a81b66c3aa2c64577916f727e1307b60c9d6afa80b6f5ca3e3049c40f643':
-    'Vanilla',
   '0x6ad07205a4d725c5c2b10c4f5fbdfaaa351c742fce7a5a22b2b56fd8d5afd62':
+    'Vanilla',
+  '0x594a81b66c3aa2c64577916f727e1307b60c9d6afa80b6f5ca3e3049c40f643':
     'Ethereum signature'
 };
 
