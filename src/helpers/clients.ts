6271201cdbad48767d8dc5ab8adc3f4ddc16b052
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-09-23 11:26:12 +0200

    feat: use ETH_RPC_URL for node URL to match mana

--- src/helpers/clients.ts
+++ src/helpers/clients.ts
@@ -2,7 +2,7 @@ import { clients as Clients } from '@snapshot-labs/sx';
 
 const manaUrl: string =
   import.meta.env.VITE_MANA_URL || 'http://localhost:3000';
-const ethUrl: string = import.meta.env.VITE_ETH_NODE_URL;
+const ethUrl: string = import.meta.env.VITE_ETH_RPC_URL;
 
 export default {
   EthereumSig: new Clients.EthereumSig({
