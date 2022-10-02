77e8d9016c8fb60189a74ba5bb4a6722e3c233a4
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2022-10-02 14:01:25 +0200

    feat: add Results labels

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -10,6 +10,12 @@ withDefaults(
     width: 100
   }
 );
+
+const labels = {
+  0: 'For',
+  1: 'Against',
+  2: 'Abstain'
+};
 </script>
 
 <template>
@@ -22,11 +28,13 @@ withDefaults(
     >
       <div
         v-if="proposal.scores_total === 0"
+        title="No votes"
         class="choice-bg _3 float-left w-full h-full"
       />
       <div
         v-for="(score, i) in Array(3)"
         :key="i"
+        :title="labels[i]"
         class="choice-bg float-left h-full"
         :style="{
           width: `${(
