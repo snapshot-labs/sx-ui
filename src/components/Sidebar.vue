e46d58d80d684ee27ddafd57f681e0d12cb9bf23
j0hnfl0w <ij69@pm.me>
2023-01-27 18:26:04 +0300

    fix: only starred spaces in sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -13,29 +13,15 @@ onMounted(() => spacesStore.fetch());
       <IH-stop class="inline-block my-4 w-[32px] h-[32px] text-skin-link" />
     </router-link>
     <UiLoading v-if="!spacesStore.loaded" class="py-2 block" />
-    <template v-else>
-      <div class="space-y-3 p-2">
-        <router-link
-          v-for="(space, i) in spacesStore.spaces.slice(0, 5)"
-          :key="i"
-          :to="{ name: 'overview', params: { id: space.id } }"
-          class="block"
-        >
-          <Stamp :id="space.id" :size="32" class="!rounded-[4px]" />
-        </router-link>
-      </div>
-      <!-- starred spaces -->
-      <IS-star v-if="spacesStore.spacesStarred.length" class="inline-block" />
-      <div class="space-y-3 p-2">
-        <router-link
-          v-for="(spaceId, i) in spacesStore.spacesStarred"
-          :key="i"
-          :to="{ name: 'overview', params: { id: spaceId } }"
-          class="block"
-        >
-          <Stamp :id="spaceId" :size="32" class="!rounded-[4px]" />
-        </router-link>
-      </div>
-    </template>
+    <div v-else class="space-y-3 p-2">
+      <router-link
+        v-for="(spaceId, i) in spacesStore.spacesStarred"
+        :key="i"
+        :to="{ name: 'overview', params: { id: spaceId } }"
+        class="block"
+      >
+        <Stamp :id="spaceId" :size="32" class="!rounded-[4px]" />
+      </router-link>
+    </div>
   </div>
 </template>
