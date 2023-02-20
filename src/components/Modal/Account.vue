225b62f0e3a7efab63556df6e62555e4c49461fe
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-02-20 12:29:38 +0100

    refactor: hard image links instead of ipfs aliases

--- src/components/Modal/Account.vue
+++ src/components/Modal/Account.vue
@@ -2,8 +2,7 @@
 import { toRefs, watch } from 'vue';
 import { getInjected } from '@snapshot-labs/lock/src/utils';
 import { shorten, explorerUrl } from '@/helpers/utils';
-import { getLocalUrl } from '@/helpers/connectorsIcons';
-import connectors from '@/helpers/connectors.json';
+import connectors, { mapConnectorId, getConnectorIconUrl } from '@/helpers/connectors';
 import { useWeb3 } from '@/composables/useWeb3';
 
 const injected = getInjected();
@@ -11,7 +10,8 @@ if (injected)
   connectors['injected'] = {
     ...connectors['injected'],
     ...injected,
-    ...{ id: 'injected' }
+    ...{ id: 'injected' },
+    icon: connectors[mapConnectorId(injected.id)]?.icon ?? injected.icon
   };
 
 const props = defineProps({ open: Boolean });
@@ -50,7 +50,7 @@ watch(open, () => (step = null));
             class="button-outline w-full flex justify-center items-center"
           >
             <img
-              :src="getLocalUrl(connector.icon)"
+              :src="getConnectorIconUrl(connector.icon)"
               height="28"
               width="28"
               class="mr-2 -mt-1"
---
+++ src/helpers/connectors.ts
@@ -0,0 +1,78 @@
+import { getUrl } from '@/helpers/utils';
+import metamaskIcon from '@/assets/connectors/metamask.png';
+import walletconnectIcon from '@/assets/connectors/walletconnect.png';
+import coinbaseIcon from '@/assets/connectors/coinbase.png';
+import portisIcon from '@/assets/connectors/portis.png';
+import gnosisIcon from '@/assets/connectors/gnosis.png';
+import argentxIcon from '@/assets/connectors/argentx.png';
+
+export default {
+  injected: {
+    id: 'injected',
+    name: 'MetaMask',
+    type: 'injected',
+    root: 'ethereum',
+    icon: metamaskIcon
+  },
+  walletconnect: {
+    id: 'walletconnect',
+    name: 'WalletConnect',
+    network: '1',
+    icon: walletconnectIcon,
+    options: {
+      rpc: {
+        '1': 'https://cloudflare-eth.com',
+        '4': 'https://eth-rinkeby.alchemyapi.io/v2/twReQE9Px03E-E_N_Fbb3OVF7YgHxoGq',
+        '42': 'https://eth-kovan.alchemyapi.io/v2/QCsM2iU0bQ49eGDmZ7-Y--Wpu0lVWXSO'
+      }
+    }
+  },
+  walletlink: {
+    id: 'walletlink',
+    name: 'Coinbase',
+    network: '1',
+    icon: coinbaseIcon,
+    options: {
+      appName: 'Snapshot',
+      darkMode: false,
+      chainId: 1,
+      ethJsonrpcUrl: 'https://cloudflare-eth.com'
+    }
+  },
+  portis: {
+    id: 'portis',
+    name: 'Portis',
+    network: '1',
+    icon: portisIcon,
+    options: {
+      dappId: '3eb93706-c71d-456b-b4eb-322ea27f7d48',
+      network: 'mainnet'
+    }
+  },
+  gnosis: {
+    id: 'gnosis',
+    type: 'gnosis',
+    name: 'Gnosis Safe',
+    icon: gnosisIcon
+  },
+  argentx: {
+    id: 'argentx',
+    name: 'Argent X',
+    type: 'injected',
+    root: 'starknet',
+    icon: argentxIcon
+  }
+};
+
+export function mapConnectorId(sourceName: string) {
+  if (sourceName === 'metamask') return 'injected';
+  if (sourceName === 'coinbase') return 'walletlink';
+
+  return sourceName;
+}
+
+export function getConnectorIconUrl(url) {
+  if (url.startsWith('ipfs://')) return getUrl(url);
+
+  return url;
+}
