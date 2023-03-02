091ec06cfd073968185cfabaad10366efbcdc622
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-03-02 11:47:11 +0100

    feat(evm): implement deploy space for Ethereum in editor (#444)
    * feat(evm): implement deploy space for Ethereum in editor
    
    * fix: verifyNetwork before deploy

--- src/networks/evm/constants.ts
+++ src/networks/evm/constants.ts
@@ -1,3 +1,5 @@
+import { shorten } from '@/helpers/utils';
+
 export const API_URL = 'https://api.thegraph.com/subgraphs/name/snapshot-labs/sx-goerli';
 
 export const SUPPORTED_AUTHENTICATORS = {
@@ -29,8 +31,51 @@ export const EXECUTORS = {
   '0xb1001fdf62c020761039a750b27e73c512fdaa5e': 'Vanilla'
 };
 
-export const EDITOR_AUTHENTICATORS = [];
+export const EDITOR_AUTHENTICATORS = [
+  {
+    address: '0x86bfa0726cba0febeee457f04b705ab74b54d01c',
+    name: 'Vanilla',
+    paramsDefinition: null
+  },
+  {
+    address: '0x37315ce75920b653f0f13734c709e199876455c9',
+    name: 'Ethereum transaction',
+    paramsDefinition: null
+  }
+];
 
-export const EDITOR_VOTING_STRATEGIES = [];
+export const EDITOR_VOTING_STRATEGIES = [
+  {
+    address: '0x395ed61716b48dc904140b515e9f682e33330154',
+    name: 'Vanilla',
+    paramsDefinition: null
+  },
+  {
+    address: '0xbbd17346378f76c1c94032594b57c93c24857b19',
+    name: 'Delegated Comp Token',
+    generateSummary: (params: Record<string, any>) => `(${shorten(params.contractAddress)})`,
+    generateParams: (params: Record<string, any>) => [params.contractAddress],
+    paramsDefinition: {
+      type: 'object',
+      title: 'Params',
+      additionalProperties: false,
+      required: ['contractAddress'],
+      properties: {
+        contractAddress: {
+          type: 'string',
+          format: 'address',
+          title: 'Contract address',
+          examples: ['0x0000…']
+        }
+      }
+    }
+  }
+];
 
-export const EDITOR_EXECUTION_STRATEGIES = [];
+export const EDITOR_EXECUTION_STRATEGIES = [
+  {
+    address: '0xb1001fdf62c020761039a750b27e73c512fdaa5e',
+    name: 'Vanilla',
+    paramsDefinition: null
+  }
+];
