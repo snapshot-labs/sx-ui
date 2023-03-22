9d63aa7840085922224a8c578f33c5854bcb9c25
j0hnfl0w <ij69@pm.me>
2023-03-22 14:40:00 +0300

    feat: my spaces page

--- src/views/Settings/Spaces.vue
+++ src/views/Settings/Spaces.vue
@@ -1,75 +1,35 @@
 <script setup lang="ts">
-import { ref, Ref } from 'vue';
-import { useContactsStore } from '@/stores/contacts';
-import { shorten, shortenAddress } from '@/helpers/utils';
+import { ref } from 'vue';
 
-const contactsStore = useContactsStore();
-
-const modalState: Ref<{
-  editContact?: any;
-}> = ref({});
-const modalOpen = ref({
-  editContact: false
-});
-
-function openModal(type: 'editContact') {
-  modalOpen.value[type] = true;
-  modalState.value[type] = null;
-}
-
-function handleContactEdit(contact) {
-  modalState.value.editContact = contact;
-  modalOpen.value.editContact = true;
-}
+const tab = ref('controlled');
+const tabs = [
+  { id: 'controlled', name: 'Controlled' },
+  { id: 'voted', name: 'Voted' },
+  { id: 'proposed', name: 'Proposed' }
+];
 </script>
 
 <template>
   <div>
-    <div class="flex">
-      <div class="flex-auto" />
-      <div class="pt-4 px-4 space-x-2">
-        <a>
-          <UiButton class="!px-0 w-[46px]" @click="openModal('editContact')">
-            <IH-user-add class="inline-block" />
-          </UiButton>
-        </a>
-      </div>
+    <!-- TODO refactor to tabs -->
+    <div class="flex flex-row w-full justify-start border-b sticky top-[72px] bg-skin-bg">
+      <h4
+        v-for="t in tabs"
+        :key="t.id"
+        class="eyebrow py-2 px-4 cursor-pointer"
+        :class="{ 'text-skin-text': t.id !== tab }"
+        @click="tab = t.id"
+        v-text="t.name"
+      />
     </div>
-    <Label label="Contacts" />
-    <div
-      v-for="contact in contactsStore.contacts"
-      :key="contact.address"
-      class="mx-4 py-3 border-b flex group"
-    >
-      <div class="flex-auto flex items-center min-w-0">
-        <Stamp :id="contact.address" type="avatar" :size="32" />
-        <div class="flex flex-col ml-3 leading-[22px] min-w-0 pr-2 md:pr-0">
-          <h4 class="text-skin-link" v-text="shorten(contact.name, 24)" />
-          <div class="text-sm truncate" v-text="shortenAddress(contact.address)" />
-        </div>
-      </div>
-      <div class="flex flex-row items-center content-center gap-x-3">
-        <a class="invisible group-hover:visible" @click="handleContactEdit(contact)">
-          <IH-pencil />
-        </a>
-        <a
-          class="invisible group-hover:visible"
-          @click="contactsStore.deleteContact(contact.address)"
-        >
-          <IH-trash />
-        </a>
-      </div>
-    </div>
-    <div v-if="!contactsStore.contacts.length" class="px-4 py-3 text-skin-link">
-      <IH-exclamation-circle class="inline-block mr-2" />
-      <span v-text="'There are no contacts here.'" />
-    </div>
+    <template v-if="tab === 'controlled'">
+      <div v-for="i in 12" :key="i" class="w-full border-b p-4">{{ i }} space i control</div>
+    </template>
+    <template v-if="tab === 'voted'">
+      <div v-for="i in 10" :key="i" class="w-full border-b p-4">{{ i }} space where i voted</div>
+    </template>
+    <template v-if="tab === 'proposed'">
+      <div v-for="i in 5" :key="i" class="w-full border-b p-4">{{ i }} space where i created a proposal</div>
+    </template>
   </div>
-  <teleport to="#modal">
-    <ModalEditContact
-      :open="modalOpen.editContact"
-      :initial-state="modalState.editContact"
-      @close="modalOpen.editContact = false"
-    />
-  </teleport>
 </template>
