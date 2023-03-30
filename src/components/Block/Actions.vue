1a5d430a61cf55288b6557d1f4c8a24e23545faa
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-03-30 11:31:27 +0200

    feat: link to execution transaction on Proposal page (#538)
    * feat: link to execution tx on Proposal page
    
    * feat: add external link icon

--- src/components/Block/Actions.vue
+++ src/components/Block/Actions.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { useActions } from '@/composables/useActions';
 import { getNetwork } from '@/networks';
+import { shorten } from '@/helpers/utils';
 import type { Proposal as ProposalType } from '@/types';
 
 const props = defineProps<{ proposal: ProposalType }>();
@@ -13,6 +14,11 @@ const finalizeProposalSending = ref(false);
 const receiveProposalSending = ref(false);
 const executeTransactionsSending = ref(false);
 const executeQueuedProposalSending = ref(false);
+
+const network = computed(() => getNetwork(props.proposal.network));
+const baseNetwork = computed(() =>
+  network.value.baseNetworkId ? getNetwork(network.value.baseNetworkId) : network.value
+);
 
 async function handleFinalizeProposalClick() {
   finalizeProposalSending.value = true;
@@ -57,10 +63,20 @@ async function handleExecuteQueuedProposalClick() {
 
 <template>
   <div class="x-block !border-x rounded-lg p-3">
-    <div v-if="proposal.completed">Proposal has been already executed</div>
+    <div v-if="proposal.execution_tx">
+      Proposal has been already executed at
+      <a
+        class="inline-flex items-center"
+        target="_blank"
+        :href="baseNetwork.helpers.getExplorerUrl(proposal.execution_tx, 'transaction')"
+      >
+        {{ shorten(proposal.execution_tx) }}
+        <IH-external-link class="inline-block ml-1"
+      /></a>
+    </div>
     <template v-else>
       <UiButton
-        v-if="getNetwork(proposal.network).hasReceive"
+        v-if="network.hasReceive"
         class="block mb-2 w-full flex justify-center items-center"
         :loading="finalizeProposalSending"
         @click="handleFinalizeProposalClick"
@@ -69,7 +85,7 @@ async function handleExecuteQueuedProposalClick() {
         Finalize proposal
       </UiButton>
       <UiButton
-        v-if="getNetwork(proposal.network).hasReceive"
+        v-if="network.hasReceive"
         class="block mb-2 w-full flex justify-center items-center"
         :loading="receiveProposalSending"
         @click="handleReceiveProposalClick"
