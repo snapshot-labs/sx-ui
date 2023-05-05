0017d93879e84a407f7258a0ba4e9227b44f42c7
less <fabien@bonustrack.co>
2023-05-06 03:00:59 +0700

    add delegate page

--- src/helpers/highlight.ts
+++ src/helpers/highlight.ts
@@ -2,7 +2,7 @@ import fetch from 'cross-fetch';
 
 const HIGHLIGHT_URL: string = import.meta.env.VITE_HIGHLIGHT_URL || '';
 
-export async function invoke(params: any, url = HIGHLIGHT_URL) {
+export async function invoke(data: any, url = HIGHLIGHT_URL) {
   const init = {
     method: 'POST',
     headers: {
@@ -12,7 +12,11 @@ export async function invoke(params: any, url = HIGHLIGHT_URL) {
     body: JSON.stringify({
       jsonrpc: '2.0',
       method: 'invoke',
-      params,
+      params: {
+        agent: 'discussions',
+        fn: 'discuss',
+        data
+      },
       id: null
     })
   };
@@ -25,15 +29,21 @@ export const domain = {
   version: '0.1.0'
 };
 
-export const discussionType = {
-  Discussion: [
-    { name: 'space', type: 'address' },
+export const discussType = {
+  Discuss: [
+    { name: 'author', type: 'address' },
+    { name: 'space', type: 'string' },
+    { name: 'category', type: 'uint256' },
     { name: 'title', type: 'string' },
     { name: 'content', type: 'string' },
-    { name: 'author', type: 'address' },
-    { name: 'created', type: 'address' }
+    { name: 'created', type: 'uint64' }
   ]
 };
+
+export async function discuss(signer, message) {
+  const unit = await sign(signer, message, discussType);
+  return invoke(unit);
+}
 
 export async function sign(signer, message, types): Promise<any> {
   const address = await signer.getAddress();
