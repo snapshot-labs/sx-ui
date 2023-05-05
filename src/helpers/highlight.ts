f35804195516e604333ae9ff3d0eeb31ae8db40f
less <fabien@bonustrack.co>
2023-05-06 03:00:59 +0700

    add delegate page

--- src/helpers/highlight.ts
+++ src/helpers/highlight.ts
@@ -1,9 +1,8 @@
 import fetch from 'cross-fetch';
-import { pin } from '@snapshot-labs/pineapple';
 
 const HIGHLIGHT_URL: string = import.meta.env.VITE_HIGHLIGHT_URL || '';
 
-export async function invoke(agent, method, args: any, url = HIGHLIGHT_URL) {
+export async function invoke(data: any, url = HIGHLIGHT_URL) {
   const init = {
     method: 'POST',
     headers: {
@@ -12,72 +11,49 @@ export async function invoke(agent, method, args: any, url = HIGHLIGHT_URL) {
     },
     body: JSON.stringify({
       jsonrpc: '2.0',
-      method: 'post_joint',
+      method: 'invoke',
       params: {
-        unit: {
-          version: '0x1',
-          messages: [
-            {
-              type: 'INVOKE_FUNCTION',
-              payload: {
-                agent,
-                method,
-                args
-              }
-            }
-          ],
-          timestamp: ~~(Date.now() / 1e3),
-          parent_units: [],
-          signature: []
-        }
+        agent: 'discussions',
+        fn: 'discuss',
+        data
       },
       id: null
     })
   };
-
   const res = await fetch(url, init);
   return (await res.json()).result;
 }
 
-export async function addCategory(data) {
-  const metadata = {
-    name: data.name,
-    about: data.about
-  };
+export const domain = {
+  name: 'snapshot-x',
+  version: '0.1.0'
+};
 
-  const { cid } = await pin(metadata);
+export const discussType = {
+  Discuss: [
+    { name: 'author', type: 'address' },
+    { name: 'space', type: 'string' },
+    { name: 'category', type: 'uint256' },
+    { name: 'title', type: 'string' },
+    { name: 'content', type: 'string' },
+    { name: 'created', type: 'uint64' }
+  ]
+};
 
-  const metadataURI = `ipfs://${cid}`;
-  const target = '0x1';
-  const methodSelector = 'add_category';
-  const args = [data.parent, metadataURI];
-  const argsAuth = [target, methodSelector, args];
-
-  return await invoke('0x2', 'authenticate', argsAuth);
+export async function discuss(signer, message) {
+  const unit = await sign(signer, message, discussType);
+  return invoke(unit);
 }
 
-export async function discuss(data) {
-  const metadata = {
-    title: data.title,
-    content: data.content
+export async function sign(signer, message, types): Promise<any> {
+  const address = await signer.getAddress();
+  const signature = await signer._signTypedData(domain, types, message);
+
+  return {
+    address,
+    signature,
+    domain,
+    types,
+    message
   };
-
-  const { cid } = await pin(metadata);
-
-  const metadataURI = `ipfs://${cid}`;
-  const target = '0x1';
-  const methodSelector = 'discuss';
-  const args = [data.author, data.category, data.parent, metadataURI];
-  const argsAuth = [target, methodSelector, args];
-
-  return await invoke('0x2', 'authenticate', argsAuth);
-}
-
-export async function vote(data) {
-  const target = '0x1';
-  const methodSelector = 'vote';
-  const args = [data.author, data.discussion, data.choice];
-  const argsAuth = [target, methodSelector, args];
-
-  return await invoke('0x2', 'authenticate', argsAuth);
 }
