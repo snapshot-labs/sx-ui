b11abe8bc201bdb2a991541b52332c9a5003ef63
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-05-08 14:44:16 +0200

    feat: use number inputs for duration

--- src/components/S/IDuration.vue
+++ src/components/S/IDuration.vue
@@ -9,10 +9,6 @@ const emit = defineEmits<{
   (e: 'update:modelValue', value: number);
 }>();
 
-const DAYS = new Array(31 + 1).fill(0).map((_, i) => i);
-const HOURS = new Array(24).fill(0).map((_, i) => i);
-const MINUTES = new Array(60).fill(0).map((_, i) => i);
-
 const days = ref(0);
 const hours = ref(0);
 const minutes = ref(0);
@@ -40,26 +36,28 @@ watch(
     <div v-text="definition.title" />
     <div class="flex gap-3">
       <SBase :definition="{ title: 'Days' }" class="flex-1">
-        <select v-model="days" class="s-input">
-          <option v-for="day in DAYS" :key="day" :value="day">
-            {{ day }}
-          </option>
-        </select>
+        <input v-model="days" class="s-input" type="number" min="0" />
       </SBase>
       <SBase :definition="{ title: 'Hours' }" class="flex-1">
-        <select v-model="hours" class="s-input">
-          <option v-for="hour in HOURS" :key="hour" :value="hour">
-            {{ hour }}
-          </option>
-        </select>
+        <input v-model="hours" class="s-input" type="number" min="0" />
       </SBase>
       <SBase :definition="{ title: 'Minutes' }" class="flex-1">
-        <select v-model="minutes" class="s-input">
-          <option v-for="minute in MINUTES" :key="minute" :value="minute">
-            {{ minute }}
-          </option>
-        </select>
+        <input v-model="minutes" class="s-input" type="number" min="0" />
       </SBase>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type='number'] {
+  appearance: textfield;
+  -moz-appearance: textfield;
+}
+</style>
