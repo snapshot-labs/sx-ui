7de68e70ade801b6dbb9447df4c4bd1fe4b7770a
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-05-11 01:15:47 +0200

    refactor: handle route params with reactivity (#635)
    * refactor: handle react params with reactivity
    
    This is continuation of Editor refactor, this time removing static
    handling of route params in other components.
    
    With those changes we can now remove workaround for :key on router
    to force remounts and instead can reuse when routes change.
    
    * chore: remove key prop for router

--- src/views/Space.vue
+++ src/views/Space.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import { useUiStore } from '@/stores/ui';
 import { useSpacesStore } from '@/stores/spaces';
-import type { NetworkID } from '@/types';
 
+const { param, networkId, address } = useRouteParser('id');
 const uiStore = useUiStore();
-const route = useRoute();
 const spacesStore = useSpacesStore();
-const id = route.params.id as string;
-const [networkId, spaceId] = id.split(':');
 
-onMounted(() => {
-  spacesStore.fetchSpace(spaceId, networkId as NetworkID);
+const space = computed(() => spacesStore.spacesMap.get(param.value));
+
+watch([networkId, address], ([networkId, address]) => spacesStore.fetchSpace(address, networkId), {
+  immediate: true
 });
 </script>
 
@@ -21,8 +20,8 @@ onMounted(() => {
         class="ml-0 lg:ml-[240px] mr-0 xl:mr-[240px]"
         :class="{ 'translate-x-[240px] lg:translate-x-0': uiStore.sidebarOpen }"
       >
-        <UiLoading v-if="!spacesStore.spacesMap.has(id)" class="block p-4" />
-        <router-view v-else :space="spacesStore.spacesMap.get(id)" />
+        <UiLoading v-if="!space" class="block p-4" />
+        <router-view v-else :space="space" />
       </div>
       <div class="invisible xl:visible fixed w-[240px] border-l bottom-0 top-[72px] right-0" />
     </div>
