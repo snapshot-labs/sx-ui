051356bccbaa2abc93fa6e1a40b8fafc888ae5a1
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-05-10 18:14:53 +0200

    chore: use rpc.snapshotx.xyz

--- src/helpers/provider.ts
+++ src/helpers/provider.ts
@@ -3,7 +3,7 @@ import { StaticJsonRpcProvider } from '@ethersproject/providers';
 const providers: Record<number, StaticJsonRpcProvider | undefined> = {};
 
 export function getProvider(networkId: number): StaticJsonRpcProvider {
-  const url = `https://rpc.brovider.xyz/${networkId}`;
+  const url = `https://rpc.snapshotx.xyz/${networkId}`;
 
   let provider = providers[networkId];
 
