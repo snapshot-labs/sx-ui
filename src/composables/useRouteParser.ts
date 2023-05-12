a25f258a696756f7e14eaaba6eda5b0c4cb2b760
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-05-12 11:49:23 +0200

    fix: handle undefined params
    It's possible that params get reset while component is still
    mounted. We need to handle that case.

--- src/composables/useRouteParser.ts
+++ src/composables/useRouteParser.ts
@@ -4,8 +4,8 @@ export function useRouteParser(paramName: string) {
   const route = useRoute();
 
   const param = computed(() => route.params[paramName] as string);
-  const networkId = computed(() => param.value.split(':')[0] as NetworkID);
-  const address = computed(() => param.value.split(':')[1]);
+  const networkId = computed(() => (param.value ? (param.value.split(':')[0] as NetworkID) : null));
+  const address = computed(() => (param.value ? param.value.split(':')[1] : null));
 
   return {
     param,
