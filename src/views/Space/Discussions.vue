d2dafc97993807544800e536ce49505a1c165c85
less <fabien@bonustrack.co>
2023-05-15 05:45:51 +0700

    feat: add scores

--- src/views/Space/Discussions.vue
+++ src/views/Space/Discussions.vue
@@ -17,6 +17,20 @@ import '@/helpers/highlight';
   </div>
   <div class="space-y-4">
     <div>
+      <Label label="Categories" sticky />
+      <a
+        v-for="(category, i) in forum.categories"
+        :key="i"
+        class="flex justify-between items-center mx-4 py-3 border-b"
+      >
+        <div class="flex-1">
+          <h3 class="text-skin-link" v-text="category.title" />
+          <div class="text-skin-text text-sm" v-text="category.about" />
+        </div>
+        <h3 v-text="_n(category.discussions)" />
+      </a>
+    </div>
+    <div>
       <Label label="Latest discussions" sticky />
       <router-link
         v-for="(discussion, i) in forum.discussions"
@@ -31,20 +45,6 @@ import '@/helpers/highlight';
         <h3 v-text="_n(discussion.comments)" />
       </router-link>
       <a class="px-4 py-2 block">See more</a>
-    </div>
-    <div>
-      <Label label="Categories" sticky />
-      <a
-        v-for="(category, i) in forum.categories"
-        :key="i"
-        class="flex justify-between items-center mx-4 py-3 border-b"
-      >
-        <div class="flex-1">
-          <h3 class="text-skin-link" v-text="category.title" />
-          <div class="text-skin-text text-sm" v-text="category.about" />
-        </div>
-        <h3 v-text="_n(category.discussions)" />
-      </a>
     </div>
   </div>
 </template>
