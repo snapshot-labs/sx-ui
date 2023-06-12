23ae3480660921f939047f97a2adacf804e5adbb
less <fabien@bonustrack.co>
2023-06-12 22:16:26 +0700

    feat: add reply form

---
+++ src/components/ReplyForm.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { faker } from '@faker-js/faker';
+import { discuss } from '@/helpers/highlight';
+
+const props = defineProps<{ reply: any }>();
+
+const { web3 } = useWeb3();
+
+const content = ref(faker.lorem.lines(1));
+const loading = ref(false);
+
+async function handleSubmit() {
+  loading.value = true;
+  const account = web3.value.account;
+
+  const result = await discuss({
+    author: account,
+    category: props.reply.category.category_id,
+    title: '',
+    content: content.value,
+    parent: props.reply.discussion_id
+  });
+
+  content.value = '';
+  loading.value = false;
+  console.log('Result', result);
+}
+</script>
+
+<template>
+  <div class="pt-3 flex">
+    <div class="w-[40px] mr-3" />
+    <div class="s-base items-center flex-1 s-box max-w-[650px]">
+      <textarea
+        v-model="content"
+        :disabled="loading"
+        class="s-input !py-[12px] h-[108px]"
+        placeholder="Type content here"
+      />
+      <UiButton :loading="loading" @click="handleSubmit">Submit</UiButton>
+    </div>
+  </div>
+</template>
