0cf63a7fd94cca7458652e72086f6dff14497076
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-06-13 14:06:52 +0200

    feat: add Dropdown (#675)
    * feat: add Dropdown
    
    * feat: make button customizable
    
    * chore: add pointer cursor to dropdown button

---
+++ src/components/Ui/DropdownItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { MenuItem } from '@headlessui/vue';
+
+defineProps<{
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <MenuItem
+    v-slot="{ active }"
+    :disabled="disabled"
+    as="template"
+    class="w-full px-3 py-2 text-sm whitespace-nowrap"
+  >
+    <slot :active="active" :disabled="disabled" />
+  </MenuItem>
+</template>
