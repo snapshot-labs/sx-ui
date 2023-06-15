c1826620ef385c86ef04c7d1b723adda2cd1783c
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-06-15 14:57:13 +0200

    feat: add execution countdown for veto strategy

--- src/networks/common/graphqlApi/types.ts
+++ src/networks/common/graphqlApi/types.ts
@@ -86,7 +86,6 @@ export type ApiProposal = {
   has_started: boolean;
   has_execution_window_opened: boolean;
   has_ended: boolean;
-  has_veto_period_ended: boolean;
   executed: boolean;
   completed: boolean;
   cancelled: boolean;
