46ea76a34fd4adfb69ad2f0eab4ac963161baa61
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-06-16 12:46:29 +0200

    feat: merge min end and max end into single step if same

--- src/components/Modal/Timeline.vue
+++ src/components/Modal/Timeline.vue
@@ -1,20 +1,62 @@
-<script setup>
+<script setup lang="ts">
 import { _t } from '@/helpers/utils';
+import { Proposal } from '@/types';
 
-defineProps({
-  open: Boolean,
-  proposal: Object
-});
+const props = defineProps<{
+  open: boolean;
+  proposal: Proposal;
+}>();
 
-defineEmits(['close']);
+defineEmits<{
+  (e: 'close'): void;
+}>();
 
 const labels = {
   created: 'Created',
   start: 'Start',
+  end: 'End',
   min_end: 'Min. end',
   max_end: 'Max. end'
 };
-const states = Object.keys(labels);
+
+const states = computed(() => {
+  if (props.proposal.min_end === props.proposal.max_end) {
+    return [
+      {
+        id: 'created',
+        value: props.proposal.created
+      },
+      {
+        id: 'start',
+        value: props.proposal.start
+      },
+      {
+        id: 'end',
+        value: props.proposal.min_end
+      }
+    ];
+  }
+
+  return [
+    {
+      id: 'created',
+      value: props.proposal.created
+    },
+    {
+      id: 'start',
+      value: props.proposal.start
+    },
+    {
+      id: 'min_end',
+      value: props.proposal.min_end
+    },
+    {
+      id: 'max_end',
+      value: props.proposal.max_end
+    }
+  ];
+});
+
 const now = parseInt((Date.now() / 1e3).toFixed());
 </script>
 
@@ -25,30 +67,22 @@ const now = parseInt((Date.now() / 1e3).toFixed());
     </template>
     <div class="p-4 flex flex-column">
       <div class="mt-1 ml-2">
-        <div
-          v-for="(state, i) in ['created', 'start', 'min_end', 'max_end']"
-          :key="state"
-          class="flex relative h-[60px]"
-        >
+        <div v-for="(state, i) in states" :key="state.id" class="flex relative h-[60px]">
           <div
             class="absolute w-[15px] h-[15px] inline-block rounded-full -left-[7px] border-[4px] border-skin-bg"
-            :class="proposal[state] <= now ? 'bg-skin-heading' : 'bg-gray-600'"
+            :class="state.value <= now ? 'bg-skin-heading' : 'bg-gray-600'"
           />
           <div
             v-if="states[i + 1]"
             class="border-l pr-4 mt-3"
-            :class="proposal[states[i + 1]] <= now && 'border-skin-heading'"
+            :class="states[i + 1].value <= now && 'border-skin-heading'"
           />
         </div>
       </div>
       <div class="flex-auto leading-6">
-        <div
-          v-for="state in ['created', 'start', 'min_end', 'max_end']"
-          :key="state"
-          class="mb-3 last:mb-0 h-[44px]"
-        >
-          <h4 v-text="labels[state]" />
-          {{ _t(proposal[state]) }}
+        <div v-for="state in states" :key="state.id" class="mb-3 last:mb-0 h-[44px]">
+          <h4 v-text="labels[state.id]" />
+          {{ _t(state.value) }}
         </div>
       </div>
     </div>
