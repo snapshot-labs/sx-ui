5a5923cf9b23cb93ebb8081a2a6b76b6077fd103
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-10-20 19:15:08 +0200

    feat: make headers sticky

--- src/views/Proposal/Votes.vue
+++ src/views/Proposal/Votes.vue
@@ -99,8 +99,8 @@ watch([sortBy, choiceFilter], () => {
         <col class="w-[60px] lg:w-[20%]" />
         <col class="w-[0px] lg:w-[100px]" />
       </colgroup>
-      <thead>
-        <tr class="border-b bg-skin-border/10">
+      <thead class="bg-skin-bg sticky top-[113px] z-50 after:border-b after:absolute after:w-full">
+        <tr class="bg-skin-border/10">
           <th class="pl-4 font-medium">Voter</th>
           <th class="hidden lg:table-cell">
             <button
