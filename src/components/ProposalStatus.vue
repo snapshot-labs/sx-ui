4ad1936f7b918f958fd86afa10fe9a3596334c18
less <fabien@bonustrack.co>
2023-11-16 16:29:41 +0700

    chore: change native currency name to Ether

--- src/components/ProposalStatus.vue
+++ src/components/ProposalStatus.vue
@@ -24,19 +24,22 @@ defineProps<{ state: ProposalState }>();
     }"
     class="inline-block rounded-full pl-2 pr-[10px] pb-[2px] text-white mb-2"
   >
-    <IS-clock v-if="state === 'pending'" class="text-white inline-block w-[17px] h-[17px]" />
+    <IS-clock
+      v-if="state === 'pending'"
+      class="text-white inline-block w-[17px] h-[17px] mb-[1px]"
+    />
     <IS-status-online
       v-else-if="state === 'active'"
-      class="text-white inline-block w-[17px] h-[17px]"
+      class="text-white inline-block w-[17px] h-[17px] mb-[1px]"
     />
     <IS-check-circle
       v-else-if="state === 'passed'"
-      class="text-skin-bg inline-block w-[17px] h-[17px]"
+      class="text-skin-bg inline-block w-[17px] h-[17px] mb-[1px]"
     />
     <IS-play v-else-if="state === 'executed'" class="text-white inline-block w-[17px] h-[17px]" />
     <IS-x-circle
       v-else-if="state === 'rejected'"
-      class="text-white inline-block w-[17px] h-[17px]"
+      class="text-white inline-block w-[17px] h-[17px] mb-[1px]"
     />
     {{ titles[state] }}
   </div>
