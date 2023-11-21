3540b58782df871c139e94dedfcca7521d1a4fc2
Sam <samuv.eth@gmail.com>
2023-11-21 13:50:39 +0700

    Fix argentx issues with modal and persistence

--- src/helpers/argentx.ts
+++ src/helpers/argentx.ts
@@ -1,4 +1,4 @@
-import { Connector } from 'use-wagmi';
+import { Connector, WalletClient } from 'use-wagmi';
 import type { ConnectOptions, StarknetWindowObject } from '@argent/get-starknet';
 import { createWalletClient, custom } from 'viem';
 import sn from 'get-starknet-core';
@@ -15,21 +15,25 @@ export class ArgentXWalletConnector extends Connector<StarknetWindowObject, Conn
   }
 
   async connect() {
-    const { connect } = await import('@argent/get-starknet');
-    const provider = await connect(this.options);
-    if (!provider) {
-      throw Error('User rejected wallet selection or silent connect found nothing');
-    }
+    let provider = await this.getProvider();
 
-    this.#provider = provider;
+    if (!provider) {
+      this.emit('message', { type: 'connecting' });
+
+      const { connect } = await import('@argent/get-starknet');
+      provider = (await connect(this.options)) as StarknetWindowObject;
+      if (!provider) {
+        throw Error('User rejected wallet selection or silent connect found nothing');
+      }
+
+      await provider.enable({ starknetVersion: 'v4' });
+      if (!provider.isConnected) throw Error('Failed to connect to wallet');
+
+      this.#provider = provider;
+    }
 
     provider.on('accountsChanged', this.onAccountsChanged);
     provider.on('networkChanged', this.onChainChanged);
-
-    this.emit('message', { type: 'connecting' });
-
-    await provider.enable({ starknetVersion: 'v4' });
-    if (!provider.isConnected) throw Error('Failed to connect to wallet');
 
     const account = provider.selectedAddress as `0x${string}`;
     const id = await this.getChainId();
@@ -52,18 +56,16 @@ export class ArgentXWalletConnector extends Connector<StarknetWindowObject, Conn
   async getProvider() {
     if (!this.#provider) {
       const wallet = await sn.getLastConnectedWallet();
-      if (!wallet) throw Error('No wallet found');
 
-      this.#clearPreviousLocalStorage();
-
-      await wallet.enable({ starknetVersion: 'v4' });
-      if (!wallet?.isConnected) throw Error('Failed to connect to wallet');
-
-      this.#provider = wallet;
+      if (wallet) {
+        await wallet.enable({ starknetVersion: 'v4' });
+        this.#provider = wallet;
+      }
     }
-    return this.#provider;
+    return this.#provider as StarknetWindowObject;
   }
 
+  async getWalletClient(): Promise<WalletClient> {
   async getWalletClient(): Promise<any> {
     const [provider, account] = await Promise.all([this.getProvider(), this.getAccount()]);
     if (!provider) throw new Error('Provider is required.');
@@ -88,23 +90,13 @@ export class ArgentXWalletConnector extends Connector<StarknetWindowObject, Conn
     }
   }
 
-  #clearPreviousLocalStorage() {
-    // When reloading the page, the previous wallet was still in localStorage
-    // this would prevent disconnecting from the wallet
-    Object.keys(localStorage)
-      .filter(sk => sk.startsWith('gsw-last'))
-      .forEach(sk => {
-        localStorage.removeItem(sk);
-      });
-  }
-
   protected onAccountsChanged = (accounts: string[]) => {
     if (accounts.length === 0) this.emit('disconnect');
     else this.emit('change', { account: accounts[0] as `0x${string}` });
   };
 
-  protected onChainChanged = (chainId: string | any) => {
-    this.emit('change', { chain: { id: chainId, unsupported: false } });
+  protected onChainChanged = (chainId?: string | number) => {
+    this.emit('change', { chain: { id: Number(chainId), unsupported: false } });
   };
 
   protected onDisconnect = async () => {
