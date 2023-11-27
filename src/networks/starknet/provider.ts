09439ae662e52b116c67b6e481be7ad67516350c
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2023-11-27 23:52:09 +0100

    feat(starknet): chore: update to post-audit deployment (#819)
    * feat(starknet): chore: update to post-audit deployment
    
    * fix: update supported authenticators address
    
    * chore: bump @snapshot-labs/sx
    
    * fix: reimplement waitForTransaction for RpcProvider
    
    * fix: decrease interval for waitForTransaction
    
    Co-authored-by: Less <bonustrack@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Less <bonustrack@users.noreply.github.com>

--- src/networks/starknet/provider.ts
+++ src/networks/starknet/provider.ts
@@ -1,15 +1,12 @@
 import { NetworkID } from '@/types';
-import { Provider } from 'starknet';
+import { RpcProvider } from 'starknet';
 
 export function createProvider(networkId: NetworkID) {
   let baseUrl: string;
-  if (networkId === 'sn-tn') baseUrl = 'https://alpha4.starknet.io';
-  else if (networkId === 'sn-tn2') baseUrl = 'https://alpha4-2.starknet.io';
+  if (networkId === 'sn-tn') baseUrl = import.meta.env.VITE_STARK_RPC_URL;
   else throw new Error('Invalid networkId');
 
-  return new Provider({
-    sequencer: {
-      baseUrl: baseUrl
-    }
+  return new RpcProvider({
+    nodeUrl: baseUrl
   });
 }
