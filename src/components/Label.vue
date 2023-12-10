7142d7d5e2acdd82de80804e042c7454d7599335
less <fabien@bonustrack.co>
2023-12-10 19:51:22 +0700

    test: fix pixel

--- src/components/Label.vue
+++ src/components/Label.vue
@@ -9,7 +9,7 @@ defineProps<{
   <h4
     class="eyebrow border-b py-2 px-4 text-skin-text"
     :class="{
-      'sticky z-10 top-[71px] lg:top-[72px] bg-skin-bg': sticky
+      'sticky z-10 top-[72px] bg-skin-bg': sticky
     }"
     v-text="label"
   />
