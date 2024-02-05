347d2efdd58b1ed4fde993df84f2cf580c0c8b2e
Sam <51686767+samuveth@users.noreply.github.com>
2024-02-05 23:57:25 +0700

    fix: Remove focus within from buttons and fix keyboard navigation (#867)
    * fix: Keyboard navigation for buttons
    
    * fix: Input outline
    
    * fix: Remove focus within from buttons
    
    * fix: Button keyboard focus
    
    * fix: Add buttons to Treasury page

--- src/components/Block/SpaceFormNetwork.vue
+++ src/components/Block/SpaceFormNetwork.vue
@@ -19,16 +19,17 @@ const availableNetworks = enabledNetworks
   <div class="mb-2">
     <h3>Space network</h3>
     <div class="grid grid-cols-auto gap-3 pt-4 mb-3">
-      <div
+      <button
         v-for="network in availableNetworks"
         :key="network.id"
+        type="button"
         :class="{ 'border-skin-link': network.id === model }"
         class="flex items-center rounded-lg border px-4 py-3 text-skin-link cursor-pointer"
         @click="model = network.id"
       >
         <img :src="getUrl(network.avatar) ?? undefined" class="w-[32px] h-[32px] mr-3 rounded-lg" />
         {{ network.name }}
-      </div>
+      </button>
     </div>
   </div>
 </template>
